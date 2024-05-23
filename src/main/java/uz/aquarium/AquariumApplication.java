17c4171363e98e003a3a7f1abae281f10495a7ef
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-05-23 15:50:16 +0500

    Initial commit

---
+++ src/main/java/uz/aquarium/AquariumApplication.java
@@ -0,0 +1,13 @@
+package uz.aquarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquariumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquariumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/aquarium/AquariumApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.aquarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquariumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
